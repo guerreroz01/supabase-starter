8267f2fa8dbeec49008a2410153e057db2ba87b7
oliver <oliver.dentistry@gmail.com>
2023-07-14 09:59:54 +0200

    supabase3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   if (isLoading || isFetching) {
     return (
       <div className="bg-slate-950 h-screen flex justify-center items-center">
-        <div className="flex flex-col gap-4 text-white">
+        <div className="flex flex-col gap-4 text-white px-4 py-2">
           <p>Loading...</p>
         </div>
       </div>
